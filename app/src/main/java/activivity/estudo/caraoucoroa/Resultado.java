c4ca686752f0689440f9d63cb7863f8ab1bc9b5b
Deigon Prates <deigon_prates@hotmail.com>
2017-12-17 21:54:38 -0300

    criando acao do botao voltar e finalizando o app

--- app/src/main/java/activivity/estudo/caraoucoroa/Resultado.java
+++ app/src/main/java/activivity/estudo/caraoucoroa/Resultado.java
@@ -3,11 +3,14 @@ package activivity.estudo.caraoucoroa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class Resultado extends AppCompatActivity {
 
     private ImageView resultado;
+    private ImageButton voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,8 @@ public class Resultado extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
 
         resultado = (ImageView) findViewById(R.id.imageView2);
+        voltar = (ImageButton) findViewById(R.id.btnVoltar);
+
 
         Intent intent =  getIntent();
 
@@ -27,5 +32,13 @@ public class Resultado extends AppCompatActivity {
         }else{
             resultado.setImageResource(R.drawable.moeda_coroa);
         }
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voltando = new Intent(Resultado.this, MainActivity.class);
+                startActivity(voltando);
+            }
+        });
     }
 }
