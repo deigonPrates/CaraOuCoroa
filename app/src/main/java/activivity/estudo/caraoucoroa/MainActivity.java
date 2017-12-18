5ce5980d0f87a53645c50ffc30a8a3d554d3bc53
Deigon Prates <deigon_prates@hotmail.com>
2017-12-17 21:35:47 -0300

    criando a estrutura da classe principal

--- app/src/main/java/activivity/estudo/caraoucoroa/MainActivity.java
+++ app/src/main/java/activivity/estudo/caraoucoroa/MainActivity.java
@@ -2,14 +2,25 @@ package activivity.estudo.caraoucoroa;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton jogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        jogar = (ImageButton) findViewById(R.id.btnJogar);
+
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
