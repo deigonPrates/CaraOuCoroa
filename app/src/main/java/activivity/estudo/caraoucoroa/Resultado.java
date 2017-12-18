ded321f0c799c02b226435b9ec987be6f506712f
Deigon Prates <deigon_prates@hotmail.com>
2017-12-17 21:52:27 -0300

    criando a logica do app

--- app/src/main/java/activivity/estudo/caraoucoroa/Resultado.java
+++ app/src/main/java/activivity/estudo/caraoucoroa/Resultado.java
@@ -1,13 +1,31 @@
 package activivity.estudo.caraoucoroa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class Resultado extends AppCompatActivity {
+
+    private ImageView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        resultado = (ImageView) findViewById(R.id.imageView2);
+
+        Intent intent =  getIntent();
+
+        Bundle bundle = intent.getExtras();
+
+        int lado = bundle.getInt("lado");
+
+        if(lado == 1){
+            resultado.setImageResource(R.drawable.moeda_cara);
+        }else{
+            resultado.setImageResource(R.drawable.moeda_coroa);
+        }
     }
 }
